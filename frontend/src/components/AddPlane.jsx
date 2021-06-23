3edbfbc7d9faf5b9ca735f1b1673871adef6e77e
AleksandrKrivonos <A.KRIVONOS@didlogic.com>
2021-06-23 12:37:49 +0300

    add-plane popup

--- frontend/src/components/AddPlane.jsx
+++ frontend/src/components/AddPlane.jsx
@@ -1,59 +1,68 @@
+import React from "react";
 import {useState} from "react";
 import {useDispatch} from "react-redux";
 
 import {addPlane} from "../redux/actions/planeActions";
 
-import "./Product.css";
+import "./AddPlane.css";
 
 const AddPlane = () => {
     const dispatch = useDispatch();
+    const [showPopup, setShowPopup] = useState(false)
     const [data, setData] = useState({
-        name:"",
-        description:"",
-        imageUrl:""
+        name: "",
+        description: "",
+        imageUrl: ""
     })
 
     const onAddHandle = () => {
         dispatch(addPlane(data))
         setData({
-            name:"",
-            description:"",
-            imageUrl:""
+            name: "",
+            description: "",
+            imageUrl: ""
         })
     }
 
     return (
-        <div className="product">
-            <div className="input__area">
-                <div className="info__name">Название</div>
-                <input type="text"
-                       name="name"
-                       className="add__input"
-                       value={data.name}
-                       onChange={(e) => setData({...data, name:e.target.value})}
-                />
-            </div>
-            <div className="input__area">
-                <div className="info__name">Ссылка на изображение</div>
-                <input type="text"
-                       name="imageUrl"
-                       className="add__input"
-                       value={data.imageUrl}
-                       onChange={(e) => setData({...data, imageUrl:e.target.value})}
-                />
-            </div>
-            <div className="input__area">
-                <div className="info__name">Описание</div>
-                <textarea
-                       name="description"
-                       className="add__input"
-                       value={data.description}
-                       onChange={(e) => setData({...data, description:e.target.value})}
-                />
-            </div>
+        <React.Fragment>
+            <div className="add__fixed__btn info__button" onClick={() => setShowPopup(true)}>Добавить в каталог</div>
+            <div className={showPopup ? "wrapper show__popup" : "wrapper"}>
+                <div className="info__area">
+                    <div className="input__area">
+                        <div className="info__area__name">Название</div>
+                        <input type="text"
+                               name="name"
+                               className="add__area__input"
+                               value={data.name}
+                               onChange={(e) => setData({...data, name: e.target.value})}
+                        />
+                    </div>
+                    <div className="input__area">
+                        <div className="info__area__name">Ссылка на изображение</div>
+                        <input type="text"
+                               name="imageUrl"
+                               className="add__area__input"
+                               value={data.imageUrl}
+                               onChange={(e) => setData({...data, imageUrl: e.target.value})}
+                        />
+                    </div>
+                    <div className="input__area">
+                        <div className="info__area__name">Описание</div>
+                        <textarea
+                            name="description"
+                            className="add__area__input add__textarea"
+                            value={data.description}
+                            onChange={(e) => setData({...data, description: e.target.value})}
+                        />
+                    </div>
 
-            <div className="info__button" onClick={onAddHandle}>Добавить</div>
-        </div>
+                    <div className="info__button" onClick={onAddHandle}>Добавить</div>
+                </div>
+
+                <div className="remove" onClick={() => {setShowPopup(false)}}>&#11015;</div>
+            </div>
+        </React.Fragment>
     );
 };
 
